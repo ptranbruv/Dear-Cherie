54fade0787c47e9644d80ca3ae0ac27300465d5a
ptranbruv <cochacyeuladay410@gmail.com>
2026-04-28 17:45:20 +0700

    ok

--- frontend/src/pages/legacy/blog-detail.jsx
+++ frontend/src/pages/legacy/blog-detail.jsx
@@ -91,6 +91,9 @@ export default function BlogDetail() {
             {section.type === 'image' && section.imageUrl && (
               <section className="mt-8 rounded-[30px] overflow-hidden border border-red-50 flex items-center justify-center">
                 <img src={section.imageUrl} alt={section.heading || "Blog Image"} className="w-full h-auto max-h-[400px] md:max-h-[550px] object-cover" />
+                 {section.caption && (
+      <p className="mt-3 text-sm italic text-slate-500 text-center">{section.caption}</p>
+    )}
               </section>
             )}
           </div>
