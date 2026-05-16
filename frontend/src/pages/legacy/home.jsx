1409d49cf527ac78aa52f61a316b952ed6207714
ptranbruv <cochacyeuladay410@gmail.com>
2026-05-16 16:43:19 +0700

    ok

--- frontend/src/pages/legacy/home.jsx
+++ frontend/src/pages/legacy/home.jsx
@@ -95,7 +95,7 @@ export default function Home() {
   return (
     <div className="w-full flex-col">
      <Helmet>
-      <title>Dear, Chérie Flower bag - Túi Hoa Cá Nhân Hoá bằng AI</title>
+      <title>"Dear, Chérie Flower bag - Túi Hoa Cá Nhân Hoá bằng AI"</title>
       <meta name="description" content="Dear Chérie - Túi hoa quà tặng độc đáo. Thiết kế túi hoa cá nhân hoá bằng AI, trang phục và dịp sử dụng của bạn."/>
      </Helmet>
 
